219a2b28119edcb2c9499824f35f1ce55fe878fb
NTrope <ju5t1n5t0n3@gmail.com>
2015-07-24 14:25:50 -0500

    Expanded Angular SPA view to include authentication form and tabs for login/register
    In the future this authentication div will disappear to reveal a user greeting when logged in
    AJAX must be added to the controller for this form still

--- oak-smoke-nightlife/public/javascripts/index.js
+++ oak-smoke-nightlife/public/javascripts/index.js
@@ -1,27 +1,58 @@
 angular.module('nightlife', [])
-    .controller('nightlifeCtrl', [function () {
-        var bars = this.bars = [];
-        this.user = {};
-        this.latLong = navigator.geolocation.getCurrentPosition(function (position) {
-            var latitude  = position.coords.latitude;
-            var longitude = position.coords.longitude;
-            return latitude + ',' + longitude;
-        });
+    .controller('NightlifeCtrl', ['$scope', function ($scope) {
+        $scope.bars = [{name: 'ayo'}];
+        $scope.user = {};
+        $scope.latLong = '';
+        $scope.tab = 0;
+        $scope.user = null;
 
-        this.findBars = function () {
-            if (this.latLong) {
-                $.ajax('/city',
-                    {
-                        type: 'GET',
-                        data: {
-                            location: this.latLong
-                        },
-                        success: function(res) {
-                            bars = res;
-                        }
+        function login() {
+            $.ajax('/login',
+                {
+                type: 'POST',
+                data: {
+                    username: $scope.username,
+                    password: $scope.password
+                },
+                success: function(res) {
+                    console.log(res);
+                }
+            })
+        }
+
+        function getLocation() {
+            navigator.geolocation.getCurrentPosition(function (position) {
+                var latitude  = position.coords.latitude;
+                var longitude = position.coords.longitude;
+                $scope.latLong = latitude + ',' + longitude;
+            });
+        }
+        function updateBars() {
+            $.ajax('/city',
+                {
+                    type: 'GET',
+                    data: {
+                        location: $scope.latLong
+                    },
+                    success: function(res){
+                        $scope.$apply(function(){
+                            $scope.bars = res;
+                        })
                     }
+                }
 
-                );
+            );
+        }
+
+        getLocation();
+
+        $scope.switchTab = function switchTabToLogin(tabCode) {
+            $scope.tab = tabCode;
+        };
+
+        $scope.findBars = function findBars() {
+            if ($scope.latLong) {
+                updateBars();
             }
         }
 
