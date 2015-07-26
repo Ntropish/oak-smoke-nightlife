53297df548d7e1c8917787c27ed4d01cf895cf8b
Justin Stone <ntropish@gmail.com>
2015-07-26 15:52:39 +0000

    Removed unused require

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 /* global require: false, module: true, process: false */
 var express = require('express');
 var path = require('path');
-var favicon = require('serve-favicon');
+//var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
